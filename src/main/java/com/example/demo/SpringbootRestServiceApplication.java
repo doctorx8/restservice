ee4d5f087611e177fefd883624bc55ed99acfb4f
Your Name <you@example.com>
2021-10-03 22:44:37 +0200

    1

---
+++ src/main/java/com/example/demo/SpringbootRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestServiceApplication.class, args);
+	}
+
+}
